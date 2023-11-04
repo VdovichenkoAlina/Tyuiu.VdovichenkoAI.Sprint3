969d57d7116242aa5fdda262c8fc1c17bf86d66b
Tyuiu.VdovichenkoAI@outlook.com <a.vdovichenko10@gmail.com>
2023-11-04 20:51:16 +0500

    Task7

---
+++ Tyuiu.VdovichenkoAI.Sprint3.Task7.V6.Test/DataServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VdovichenkoAI.Sprint3.Task7.V6.Lib;
+
+namespace Tyuiu.VdovichenkoAI.Sprint3.Task7.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = 22.40;
+            valueWaitArray[1] = 20.22;
+            valueWaitArray[2] = 14.59;
+            valueWaitArray[3] = 7.21;
+            valueWaitArray[4] = 1.25;
+            valueWaitArray[5] = -2.50;
+            valueWaitArray[6] = -6.25;
+            valueWaitArray[7] = -12.21;
+            valueWaitArray[8] = -19.59;
+            valueWaitArray[9] = -25.22;
+            valueWaitArray[10] = -27.40;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.VdovichenkoAI.Sprint3.Task7.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VdovichenkoAI.Sprint3.Task7.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VdovichenkoAI.Sprint3.Task7.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b6bf288-2d53-48c8-91c0-3a4bf26fb797")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
